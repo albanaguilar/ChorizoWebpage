416bbe26a763c9d4d2954c3c919f1e57c6142d2e
albanaguilar <albanaguilar1@gmail.com>
2020-06-19 19:26:31 -0500

    nav and slider working, stable version

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+//LOADER
+var loader;
+function loadNow(opacity) {
+    if (opacity <= 0) {
+        displayContent();
+    } else {
+        loader.style.opacity = opacity;
+        window.setTimeout(function() {
+            loadNow(opacity - 0.05);
+        }, 100);
+    }
+}
+
+function displayContent() {
+    loader.style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loader = document.getElementById('loader');
+    loadNow(1);
+});
+//END LOADER
+
+//NAVBAR FOR MOBILE
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
+
+//END NAVBAR
+
+
+//SLIDER
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems);
+});
+// END SLIDER
+
+
+
+
+
+
+
+
+
+
+
+
+
